3929306f5c16103e2177d57d454c38fed79edde5
Arya0709 <97951072+Arya0709@users.noreply.github.com>
2023-06-10 16:45:06 +0530

    Add files via upload
    The sum of three numbers or integers.

---
+++ SumOfThree.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SumOfThree {
+    public static void main(String[] args){
+        Scanner read= new Scanner(System.in);
+        int a=read.nextInt();
+        int b=read.nextInt();
+        int c=read.nextInt();
+        System.out.print("Sum of Three numbers is: "+ (a+b+c));
+    }
+}
